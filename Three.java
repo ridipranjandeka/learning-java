996929244de50d418470ddd0ed95e145329ebe52
ridip <ridipranjandeka35@gmail.com>
2024-03-19 14:03:49 +0530

    threesome

---
+++ Three.java
@@ -0,0 +1,9 @@
+public class Three {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int c = 8;
+        int sum = a + b + c;
+        System.out.println(sum); 
+    }
+}
